0f9ebcceb3e1a2f013dca1df0bdc6110c517cf2f
wi23b006 <wi23b006@technikum-wien.at>
2024-07-08 16:27:41 +0200

    Implement Player and Board classes corrected commit

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Board {
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y)) {
+            cells[x][y] = marker;
+        }
+    }
+
+    public void clear() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cells[i][j] = ' ';
+            }
+        }
+    }
+    public void print() {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j] + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("---------");
+    }
 }
--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class TicTacToe {
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+    }
+
+    public void start() {
+        // Game start logic
+    }
+
+    private void switchCurrentPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.start();
+    }
 }
